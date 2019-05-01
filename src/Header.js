94c440d4b82f9b96a2df954da03951fceec9e2d0
nick <nmdufour@gmail.com>
2019-05-01 07:16:10 -0400

    first commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <header role="banner" className="heading">
+                <h1>X-O</h1>
+                <p>A tic-tac-toe game</p>
+            </header>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+);
+
+export default Header;
