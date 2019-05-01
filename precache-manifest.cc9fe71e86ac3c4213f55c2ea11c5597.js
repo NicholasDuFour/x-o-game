1199c813f44b223253c6a3d8be40cfad7ff16f05
nick <nmdufour@gmail.com>
2019-05-01 07:22:22 -0400

    Updates

---
+++ precache-manifest.cc9fe71e86ac3c4213f55c2ea11c5597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e6ad9bdec0ca35771335d1206a9b85",
+    "url": "/x-o-game/index.html"
+  },
+  {
+    "revision": "6c6672f2df31da74ac4f",
+    "url": "/x-o-game/static/css/main.15873ec3.chunk.css"
+  },
+  {
+    "revision": "52fb06c9b6d5dc299753",
+    "url": "/x-o-game/static/js/2.58e84392.chunk.js"
+  },
+  {
+    "revision": "6c6672f2df31da74ac4f",
+    "url": "/x-o-game/static/js/main.b25c658e.chunk.js"
+  },
+  {
+    "revision": "e5ab63f4a0e4808914dd",
+    "url": "/x-o-game/static/js/runtime~main.4d4a41cc.js"
+  }
+]);
